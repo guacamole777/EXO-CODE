7a6a8da50b24a1e26e8ce32e3399cb7d96784916
Sebastian Cabrol <cabrol@mymail.mines.edu>
2018-05-03 12:48:43 -0600

    updated exoCode

--- ExoCode/ExoCode.ino
+++ ExoCode/ExoCode.ino
@@ -4,34 +4,47 @@
 #include "HX711.h"
 
 
-
-int leftEncoder = 0;
-int rightEncoder = 1;
-int leftPotentiometer = 2;
-int rightPotentiometer = 3;
-int leftLoadCellCLK = 2;
-int leftLoadCell = 3;
-int rihgtLoadCellCLK = 2;
-int rightLoadCell = 5;
-
+// Encoder pins and variables
+const int leftEncoder = A0;
+const int rightEncoder = A1;
 float leftEncoderPos = 0;
 float leftEncoderDis = 0;
 float rightEncoderPos = 0;
 float rightEncoderDis = 0;
-float leftActuatorPos = 0;
-float rightActuatorPos = 0;
+
+const int leftLoadCellCLK = 2;
+const int leftLoadCell = 3;
+const int rihgtLoadCellCLK = 4;
+const int rightLoadCell = 5;
+
+const int leftActuatorPWM = 6;
+const int rightActuatorPWM = 9;
+const int leftActuatorDir = 7;
+const int rightActuatorDir = 8;
 
 const float displacementToMove = .25;
 
 float loadWeight = 0;
 
+const int UP = 0;
+const int DOWN = 1;
+const boolean LEFT = true;
+const boolean RIGHT = false;
+
+boolean moving = false;
+
 void setup() {
   pinMode(leftEncoder, INPUT);
   pinMode(rightEncoder, INPUT);
-  pinMode(leftPotentiometer, INPUT);
-  pinMode(rightPotentiometer, INPUT);
+
   pinMode(leftLoadCell, INPUT);
   pinMode(rightLoadCell, INPUT);
+
+  pinMode(leftActuatorDir, OUTPUT);
+  pinMode(rightActuatorDir, OUTPUT);
+  pinMode(leftActuatorPWM, OUTPUT);
+  pinMode(rightActuatorPWM, OUTPUT);
+
 
   
   
@@ -39,14 +52,22 @@ void setup() {
 }
 
 void loop() {
-  updateDisplacementSpring();
-  if(leftEncoderDis >= displacementToMove) {
-    // Move left Actuator
+  if (moving) {
+    
   }
-  if(rightEncoderDis >= displacementToMove) {
-    // Move right Actuator
+  else {
+    updateDisplacementSpring();
+    if(leftEncoderDis > displacementToMove) {
+      moveActuator(LEFT, UP);    
+    } else if (-leftEncoderDis > displacementToMove) {
+      moveActuator(LEFT, DOWN);    
+    }
+    if(rightEncoderDis > displacementToMove) {
+     moveActuator(RIGHT, UP);    
+    } else if (-rightEncoderDis > displacementToMove) {
+      moveActuator(RIGHT, UP);    
+    }
   }
-  
 }
 
 void updateDisplacementSpring() {
@@ -56,20 +77,24 @@ void updateDisplacementSpring() {
   rightEncoderDis = rightEncoderPos - currentPosRight;
 }
 
-float loadCellForce(int pin) {
+float leftLoadCellForce() {
    
 }
 
-void calibrateLoadCell(int pin) {
+float rightLoadCellForce() {
+   
+}
+
+void leftCalibrateLoadCell() {
   
 }
 
-void updateActuatorPos() {
-  leftActuatorPos = analogRead(leftPotentiometer) * 20.32 / 1023;
-  rightActuatorPos = analogRead(rightPotentiometer) * 20.32 / 1023;
+void rightCalibrateLoadCel() {
+  
 }
 
-void moveActuator(int pin, int speed) {
+
+void moveActuator(int pin, int dir) {
   
 }
 
